7ae1672e6f8e7bcbe4d35f2136e472acab226c14
Michael Kim <m.kim4247@gmail.com>
2018-09-26 01:35:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] 
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end 
 end 
 
